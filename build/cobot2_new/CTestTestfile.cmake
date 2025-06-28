b0340afd9fe50a514586dfde38f1e451b70b78db
Babras369 <babrasashutosh23@gmail.com>
2025-06-28 16:40:23 +0530

    built the project

--- build/cobot2_new/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lenovo/ultramotive_ws/src/cobot2_new
-# Build directory: /home/lenovo/ultramotive_ws/build/cobot2_new
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/cobot2_new/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/lenovo/ultramotive_ws/install/cobot_trial2_description/share/cobot_trial2_description/package.sh"
